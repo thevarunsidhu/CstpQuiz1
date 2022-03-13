b2c207191aa18b8191b7cd092540852a6f2323ea
Varun Sidhu <thevarunsidhu@gmail.com>
2022-03-12 23:05:25 -0800

    2nd attempt code added

---
+++ Quiz1/Quiz1/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  Quiz1
+//
+//  Created by Varun Sidhu on 2022-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var name: String = ""
+    @State var nationality: String = ""
+    @State var submit = false
+    
+    var body: some View {
+        
+        VStack {
+            Text("Every Government Form Ever")
+                .font(.system(size: 30))
+            
+            HStack {
+
+                Text("Name: ")
+                    .font(.system(size:20))
+
+                TextField("Name ", text: $name)
+                    .frame(width: 200, height: 30)
+
+            }
+
+            HStack {
+
+                Text("Nationality: ")
+                    .font(.system(size:20))
+
+                TextField("Nationality ", text: $nationality)
+                    .frame(width: 200, height: 30)
+
+            }
+            
+      
+            if submit {
+                Text(name)
+                Text(nationality)
+            }
+            
+            HStack {
+                
+                Button(action:{submit.toggle()},
+                       label: {
+                            Text("Submit")
+                                .frame(width: 90, height:30, alignment: .center)
+                })
+                
+                Button(action:{}, 
+                label: {
+                            Text("+")
+                                .frame(width: 25, height:30)
+                })
+                
+            }
+            
+            
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}z
---
+++ Quiz1/Quiz1/Quiz4App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz1App.swift
+//  Quiz1
+//
+//  Created by Varun Sidhu on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct Quiz1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
